f7a34775ac7562d3f52e30943e9f9032067ed363
Var1035 <129356069+Var1035@users.noreply.github.com>
2024-06-10 17:46:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,40 +1,102 @@
 import streamlit as st
-import requests
+from streamlit_examples.utils.cohere import summarize
+from streamlit_examples.utils.weaviate import search_wikipedia
 
-# Function to get search results from Wikipedia
-def get_wikipedia_search_results(search_term):
-    endpoint = "https://en.wikipedia.org/w/api.php"
-    params = {
-        'action': 'query',
-        'list': 'search',
-        'srsearch': search_term,
-        'format': 'json'
-    }
-    response = requests.get(endpoint, params=params)
-    return response.json()['query']['search'][:3]
+from streamlit_examples.utils.theme import (
+    initPage,
+)
 
-# Streamlit app layout
-st.title('Wikipedia Search App')
+initPage("Search Wikipedia")
 
-# Search input
-search_term = st.text_input('Enter a search term:', '')
 
-# Search button
-if st.button('Search'):
-    if search_term:
-        # Get the top 3 search results from Wikipedia
-        results = get_wikipedia_search_results(search_term)
+def link(i, item):
+    return f"**[{i+1}. {item['title']}]({item['url']})**"
 
-        # Display the results
-        for result in results:
-            title = result['title']
-            snippet = result['snippet']
-            url = f"https://en.wikipedia.org/?curid={result['pageid']}"
 
-            # Display each result as a clickable link with a snippet
-            st.markdown(f"### {title}")
-            st.markdown(snippet)
-    else:
-        st.error('Please enter a search term.')
+def aggregate(items):
+    # group items by same url
+    groups = {}
+    for item in items:
+        groups.setdefault(item["url"], []).append(item)
+    # join text of same url
+    results = []
+    for group in groups.values():
+        result = {}
+        result["url"] = group[0]["url"]  # get url from first item
+        result["title"] = group[0]["title"]  # get titl from first item
+        result["text"] = "\n\n".join([item["text"] for item in group])
+        results.append(result)
+    return results
 
-# Run the Streamlit app by saving this script and running streamlit run your_script.py
+
+def render_suggestions():
+    def set_query(query):
+        st.session_state.suggestion = query
+
+    suggestions = [
+        "Travel destinations known for their beaches",
+        "Time travel movies with a twist",
+        "Book authors explaining Physics",
+    ]
+    columns = st.columns(len(suggestions))
+    for i, column in enumerate(columns):
+        with column:
+            st.button(suggestions[i], on_click=set_query, args=[suggestions[i]])
+
+
+def render_query():
+    st.text_input(
+        "Search",
+        placeholder="Search, e.g. 'Backpacking in Asia'",
+        key="user_query",
+        label_visibility="collapsed",
+    )
+
+
+def get_query():
+    if "suggestion" not in st.session_state:
+        st.session_state.suggestion = None
+    if "user_query" not in st.session_state:
+        st.session_state.user_query = ""
+    user_query = st.session_state.suggestion or st.session_state.user_query
+    st.session_state.suggestion = None
+    st.session_state.user_query = ""
+    return user_query
+
+
+user_query = get_query()
+if not user_query:
+    st.info(
+        "Search Wikipedia and summarize the results. Type a query to start or pick one of these suggestions:"
+    )
+render_suggestions()
+render_query()
+
+if not user_query:
+    st.stop()
+
+MAX_ITEMS = 3
+
+container = st.container()
+header = container.empty()
+header.write(f"Looking for results for: _{user_query}_")
+placeholders = []
+for i in range(MAX_ITEMS):
+    placeholder = container.empty()
+    placeholder.status("Searching...")
+    placeholders.append(placeholder)
+
+items = search_wikipedia(user_query, limit=10)
+# aggregate items with same url (as vector DB might return multiple items for same url)
+items = aggregate(items)[:MAX_ITEMS]
+
+header.write(f"That's what I found about: _{user_query}_. **Summarizing results...**")
+for i, item in enumerate(items):
+    placeholders[i].info(f"{link(i,item)} {item['text']}")
+
+for i, item in enumerate(items):
+    with placeholders[i].status(f"_Summarizing_: {link(i,item)} {item['text']}"):
+        summary = summarize(item["text"])
+    placeholders[i].success(f"{link(i,item)} {summary}")
+
+header.write("Search finished. Try something else!")
